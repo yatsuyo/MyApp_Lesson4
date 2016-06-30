bbc362edd9b4cbc356d85945c28c128031547c96
yatsuyo <hungry_soul_0122@yahoo.co.jp>
2016-07-01 01:18:05 +0900

    Lesson4_5.4

--- app/src/main/java/com/example/admin/myapp_lesson4/MainActivity.java
+++ app/src/main/java/com/example/admin/myapp_lesson4/MainActivity.java
@@ -11,12 +11,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 整数型の変数をnumという名前で作成して、10を代入する
-        int num = 10;
-        Log.d("javatest", String.valueOf(num));
+        int num1 = 10 + 5 -2 * 4 / 2;
+        Log.d("javatest", "10 + 5 - 2 * 4 / 2 = " + String.valueOf(num1));
 
-        // numに50を代入する
-        num = 50;
-        Log.d("javatest", String.valueOf(num));
+        boolean flag1 = true;
+        boolean flag2 = false;
+        Log.d("javatest", "flag1 && flag2 = " + String.valueOf(flag1 && flag2));
+        Log.d("javatest", "flag1 || flag2 = " + String.valueOf(flag1 || flag2));
+
+        int num2 = 10;
+        int num3 = 20;
+        Log.d("javatest", "num2 < num3 = " + String.valueOf(num2 < num3));
+
     }
 }
